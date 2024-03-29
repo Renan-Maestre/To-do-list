c55178f5a4275950034fc01e4defec08b97ff191
Renan-Maestre <renan.maestre.dev@gmail.com>
2024-03-29 15:33:41 -0300

    adicionar lista

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const localStoragekey = 'to-do-list'
+const localStoragekey = 'to-do-list-rm'
 
 function novatarefa(){
     let input = document.getElementById('input--new--tarefa')
@@ -11,7 +11,34 @@ function novatarefa(){
     else
     {
         //adicionando em LocalStorege
-        let values =  JSON.parse(localStorage.getItem)
+        let values =  JSON.parse(localStorage.getItem(localStoragekey) || "[]")
+        values.push({
+            name: input.value
+        })
+        localStorage.setItem(localStoragekey,JSON.stringify(values))
+        MostrarValores()
     }
     
+    function MostrarValores()
+    {
+        let values =  JSON.parse(localStorage.getItem(localStoragekey) || "[]")
+        let list = document.getElementById('to-do-list')
+        list.innerHTML = ''
+        for(let i = 0; i < values.length; i++)
+        {
+            list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'>ok</button></li>`
+        }
+    }
+
+    function removeIten(data)
+    {
+        let values =  JSON.parse(localStorage.getItem(localStoragekey) || "[]")
+        let index = values.findIndex(x => x.name == data)
+        values.splice(index,1)
+        localStorage.setItem(localStoragekey,JSON.stringify(values))
+        MostrarValores()
+    }
+
+    MostrarValores()
+
 }
